fb9a933c13d6667c48610a59393c5adce070d125
Micko Silvestre <mckslvstr@gmail.com>
2023-12-15 22:01:27 +0800

    Splitted the fail scenarios into their own tests. Added some comments on what each tests is about.

--- tests/ConfigurationManagerTests/JsonTests.cs
+++ tests/ConfigurationManagerTests/JsonTests.cs
@@ -11,25 +11,69 @@ namespace ConfigurationManagerTests
             _configurationManager = new ConfigurationManager();
         }
 
+        /// <summary>
+        ///     This should depict a normal happy path for loading a JSON configuration file.
+        ///     If everything is as it should be then the environment should be set to the proper value and the key should be found.
+        /// </summary>
+        /// <param name="environment"></param>
+        /// <param name="expectedOutput"></param>
+        /// <param name="key"></param>
         [Test]
-        public void LoadJsonSuccess()
+        [TestCase("Dev", "dev-value", "SomeKey")]
+        public void LoadJsonSuccess(string environment, string expectedOutput, string key)
+        {
+            Assert.DoesNotThrow(() => _configurationManager.LoadConfiguration(Path.Combine("..", "..", "..", "Files", "appsettings.json")));
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(_configurationManager.GetCurrentEnvironment(), Is.EqualTo(environment), "Environment was not set correctly.");
+                Assert.That(_configurationManager.GetConfiguration<string>(key), Is.EqualTo(expectedOutput), $"{key} value is incorrect.");
+            });
+        }
+
+        /// <summary>
+        ///     This is for when the file was not found.
+        /// </summary>
+        [Test]
+        public void LoadJsonFileNotFound()
+        {
+            Assert.Throws<FileNotFoundException>(() => _configurationManager.LoadConfiguration(Path.Combine("..", "..", "..", "Files", "fails.json")));
+        }
+
+        /// <summary>
+        ///    This is for when the file extension is not supported.
+        /// </summary>
+        [Test]
+        public void LoadJsonNotSupported()
+        {
+            Assert.Throws<NotSupportedException>(() => _configurationManager.LoadConfiguration(Path.Combine("..", "..", "..", "Files", "appsettings.DHO")));
+        }
+
+        /// <summary>
+        ///     This is for when the key was not found even though the environment was set correctly.
+        /// </summary>
+        [Test]
+        public void LoadJsonKeyNotFound()
         {
             Assert.DoesNotThrow(() => _configurationManager.LoadConfiguration(Path.Combine("..", "..", "..", "Files", "appsettings.json")));
 
             Assert.Multiple(() =>
             {
                 Assert.That(_configurationManager.GetCurrentEnvironment(), Is.EqualTo("Dev"), "Environment was not set correctly.");
-                Assert.That(_configurationManager.GetConfiguration<string>("SomeKey"), Is.EqualTo("dev-value"), "SomeKey value is incorrect.");
+                Assert.Throws<KeyNotFoundException>(() => _configurationManager.GetConfiguration<string>("SecretKey"));
             });
         }
 
+        /// <summary>
+        ///     These are for some generic exceptions that could be thrown.
+        /// </summary>
+        /// <param name="fileName"></param>
         [Test]
-        public void LoadJsonFails()
+        [TestCase("configurationsnotfound.json")]
+        [TestCase("confignotspecified.json")]
+        public void LoadJsonCommon(string fileName)
         {
-            Assert.Throws<FileNotFoundException>(() => _configurationManager.LoadConfiguration(Path.Combine("..", "..", "..", "Files", "fails.json")));
-            Assert.Throws<NotSupportedException>(() => _configurationManager.LoadConfiguration(Path.Combine("..", "..", "..", "Files", "appsettings.DHO")));
-            Assert.Throws<Exception>(() => _configurationManager.LoadConfiguration(Path.Combine("..", "..", "..", "Files", "configurationsnotfound.json")));
-            Assert.Throws<Exception>(() => _configurationManager.LoadConfiguration(Path.Combine("..", "..", "..", "Files", "confignotspecified.json")));
+            Assert.Throws<Exception>(() => _configurationManager.LoadConfiguration(Path.Combine("..", "..", "..", "Files", fileName)));
         }
     }
 }
